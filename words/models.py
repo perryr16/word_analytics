98afe0a867f1a2a76995d705fb3cd196886d761c
Ross Perry <perryr16@gmail.com>
2021-01-22 14:00:32 -0700

    begin setting up rest endpoints

--- words/models.py
+++ words/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Word(models.Model):
+  word = models.CharField(max_length=99)
+  length = models.IntegerField()
+
+class Article(models.Model):
+  url = models.TextField()
+  title = models.TextField()
+  words = models.ManyToManyField(Word, through='ArticleWord')
+
+class ArticleWord(models.Model):
+  word = models.ForeignKey(Word, on_delete=models.CASCADE)
+  article = models.ForeignKey(Article, on_delete=models.CASCADE)
+  count = models.IntegerField(blank=True, null=True)
---
+++ words/serializers.py
@@ -0,0 +1,4 @@
+from rest_framework import serializers
+from words.models import Word, Article, ArticleWord
+
+class WordSerializer(serializers.)
