0a5cf5ffae952e0d65751058cd2259a7aa415e74
Ross Perry <perryr16@gmail.com>
2021-01-22 14:36:13 -0700

    post request funtional

--- words/views.py
+++ words/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response 
 from words.models import Word, Article, ArticleWord
+from words.serializers import WordSerializer
 
 # Create your views here.
 def home(request):
@@ -10,12 +11,21 @@ def home(request):
   return render(request, 'words/index.html', tmpl_vars)
 # I DONT THINK THIS IS CORRECT 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def word_index(request):
   if request.method == 'GET':
     words = Word.objects.all()
     serializer = WordSerializer(words, many=True)
     return Response(serializer.data)
+  if request.method == 'POST':
+    param = request.query_params['word']
+    new_word = Word(word=param, length=len(param))
+    new_word.save()
+    
+    # import pdb; pdb.set_trace()
+    # request.method 
+    # request.query_params['imput_param']
+    # request.headers
 
 
 @api_view(['GET'])
@@ -28,3 +38,4 @@ def word_show(request, pk):
   if request.method == 'GET':
     serializer = WordSerializer(word)
     return Response(serializer.data)
+
