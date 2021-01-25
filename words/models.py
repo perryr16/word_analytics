0b80fca8048e1eaf71d3938a1310e08b541561bd
Ross Perry <perryr16@gmail.com>
2021-01-25 14:57:22 -0700

    split words and articles into 2 methods

--- words/models.py
+++ words/models.py
@@ -11,6 +11,22 @@ class Word(models.Model):
 class ArticleManager(models.Manager):
   def create_article(self, title, body):
     article = self.create(title=title)
+    word_list = self.word_list(body)
+    for word in word_list:
+      try: 
+        new_word = Word(word=word, length=len(word))
+        new_word.save()
+
+      except: 
+        pass
+      
+      try: 
+        a_w = ArticleWord.objects.filter(word=word)
+        count = a_w.count + 1
+        article.words.add(new_word, through_defaults={'count':'1'})
+        article.save()
+
+
     import pdb; pdb.set_trace()
     return article
 
@@ -26,6 +42,10 @@ class ArticleManager(models.Manager):
       body = body.replace(small, ' ')
 
     word_list = body.split(' ')
+
+    for word in word_list:
+      if '\\' in word:
+        word_list.remove(word)
     return word_list
 
 
@@ -38,60 +58,55 @@ class Article(models.Model):
   def __str__(self):
     return self.url
   
-  def sort_words(body):
-    body = str(body).lower()
-    # body = self.repalce_words(body)
-    punctuation = ['(', ')', '.', ',', ':', ',', '"', '[', ']', '/']
-    for element in punctuation:
-      body = body.replace(element, '')
-
-    small_words = [' a ', ' an ', ' and ', ' are ', ' as ', ' be ', ' by ', ' for ', ' i ', ' in ', ' is ', ' it ', ' of ', ' or ', ' to ', ' the ']
-    for small in small_words:
-      body = body.replace(small, ' ')
-
-    word_list = body.split(' ')
-    word_dict = {}
-
-    for word in word_list: 
-      if "\\" not in word:
-        word_dict[word] = word_dict.get(word, 0) + 1
-    word_sort = dict(sorted(word_dict.items(), key=lambda item: item[1], reverse = True))
-    return word_sort
-
-  def create_words(body):
-    body = str(body).lower()
-    punctuation = ['(', ')', '.', ',', ':', ',', '"', '[', ']', '/']
-    for element in punctuation:
-      body = body.replace(element, '')
-
-    small_words = [' a ', ' an ', ' and ', ' are ', ' as ', ' be ', ' by ', ' for ', ' i ', ' in ', ' is ', ' it ', ' of ', ' or ', ' to ', ' the ']
-    for small in small_words:
-      body = body.replace(small, ' ')
-
-    word_list = body.split(' ')
-    import pdb; pdb.set_trace()
-    for word in word_list:
-      if Word.objects.filter(word=word):
-        a_w = ArticleWord.objects.filter(word=word)
-      else: 
-        a_w = ArticleWord(word=word, count=1)
-        a_w.count += 1
-        a_w.save()
-      
-        
-      word = Word(word=word, length=len(word))
-      
-      article_word = None
-
-
-
-  # def replace_words(self, **kwargs):
-  #   replacements = ['(', ')', '.', ',', ':', '[', ']', '/', 'a', 'i', 'to', 'an', 'the', 'in']
-  #   for element in replacements:
-  #     body.replace(element, '')
-  #   return string
 
 class ArticleWord(models.Model):
   word = models.ForeignKey(Word, on_delete=models.CASCADE)
   article = models.ForeignKey(Article, on_delete=models.CASCADE)
   count = models.IntegerField(blank=True, null=True)
+
+
+
+  # def create_words(body):
+  #   body = str(body).lower()
+  #   punctuation = ['(', ')', '.', ',', ':', ',', '"', '[', ']', '/']
+  #   for element in punctuation:
+  #     body = body.replace(element, '')
+
+  #   small_words = [' a ', ' an ', ' and ', ' are ', ' as ', ' be ', ' by ', ' for ', ' i ', ' in ', ' is ', ' it ', ' of ', ' or ', ' to ', ' the ']
+  #   for small in small_words:
+  #     body = body.replace(small, ' ')
+
+  #   word_list = body.split(' ')
+  #   import pdb; pdb.set_trace()
+  #   for word in word_list:
+  #     if Word.objects.filter(word=word):
+  #       a_w = ArticleWord.objects.filter(word=word)
+  #     else: 
+  #       a_w = ArticleWord(word=word, count=1)
+  #       a_w.count += 1
+  #       a_w.save()
+      
+        
+  #     word = Word(word=word, length=len(word))
+      
+  #     article_word = None
+
+    # def sort_words(body):
+    #   body = str(body).lower()
+    # # body = self.repalce_words(body)
+    # punctuation = ['(', ')', '.', ',', ':', ',', '"', '[', ']', '/']
+    # for element in punctuation:
+    #   body = body.replace(element, '')
+
+    # small_words = [' a ', ' an ', ' and ', ' are ', ' as ', ' be ', ' by ', ' for ', ' i ', ' in ', ' is ', ' it ', ' of ', ' or ', ' to ', ' the ']
+    # for small in small_words:
+    #   body = body.replace(small, ' ')
+
+    # word_list = body.split(' ')
+    # word_dict = {}
+
+    # for word in word_list: 
+    #   if "\\" not in word:
+    #     word_dict[word] = word_dict.get(word, 0) + 1
+    # word_sort = dict(sorted(word_dict.items(), key=lambda item: item[1], reverse = True))
+    # return word_sort
