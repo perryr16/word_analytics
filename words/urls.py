9e58dd7e6e5858e0ba4c4cf91ac6a4f771745fb7
Ross Perry <perryr16@gmail.com>
2021-01-28 13:43:19 -0700

    refactor

--- words/urls.py
+++ words/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('<int:pk>', views.word_show, name='word_show'),
+  path('db/<int:pk>', views.word_show, name='word_show'),
   path('', views.word_index, name='word_index'),
-  path('article', views.article, name='article'),
-  path('articles', views.article_index, name='article_index'),
-  path('article/<int:pk>', views.article_get, name='article_get'),
+  path('db/article', views.article, name='article'),
+  path('db/articles', views.article_index, name='article_index'),
+  path('db/article/<int:pk>', views.article_get, name='article_get'),
   path('artsy/<int:pk>', views.article_show, name='article_show'),
   path('artsy', views.article_all, name='article_all'),
   path('article_chart/<int:pk>', views.article_chart, name='article_chart'),
